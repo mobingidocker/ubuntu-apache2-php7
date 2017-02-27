51729bc4aa5d36827d75ed13635ef1cd212f2d1c
Masafumi Noguchi <ngratin@gmail.com>
2017-02-27 16:17:45 +0900

    Modify stdout path

--- script/run.sh
+++ script/run.sh
@@ -2,7 +2,7 @@
 
 echo "installing" > /var/log/container_status
 
-bash /tmp/init/init.sh 2>&1 >> /var/log/startup.log
+bash /tmp/init/init.sh >>/var/log/startup.log 2>&1
 
 echo "complete" > /var/log/container_status
 
