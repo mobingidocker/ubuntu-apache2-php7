e00c971eaba767074133c5765958c178114ebd6d
Masafumi Noguchi <ngratin@gmail.com>
2017-02-27 16:01:04 +0900

    Restructure path

--- 7.1/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "installing" > /var/log/container_status
-
-bash /tmp/init/init.sh 2>&1 >> /var/log/startup.log
-
-echo "complete" > /var/log/container_status
-
-exec /usr/bin/supervisord
