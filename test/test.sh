faa7678d4ae2fa8b7c48815e5e2293978a3b59e0
Masafumi Noguchi <ngratin@gmail.com>
2017-02-27 16:09:03 +0900

    Fix path

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
 echo "ADD code /srv/code" >> Dockerfile.${PHP_VERSION}
 
-docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../${PHP_VERSION}
+docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
 
 CID=`docker run -d -p 80:80 ci/test:${PHP_VERSION}`
