cfee57b04b3dbc78357e4553640414ad6b5b37ee
Masafumi Noguchi <ngratin@gmail.com>
2017-09-06 17:59:22 +0900

    Fix test

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,8 @@ PHP_VERSION=$1
 cd `dirname $0`
 
 echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
-echo "ADD code /srv/code" >> Dockerfile.${PHP_VERSION}
+echo "RUN rm -f /var/www/html/index.html" >> Dockerfile.${PHP_VERSION}
+echo "ADD code /var/www/html" >> Dockerfile.${PHP_VERSION}
 
 docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
